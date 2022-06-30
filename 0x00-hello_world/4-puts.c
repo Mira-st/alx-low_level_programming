1a27339400778e2d89460656d8f9ae77d79964d2
orirekemi <orirekemi64@gmail.com>
2022-06-30 08:19:49 -0700

    Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
  * Description: main-Prints out astring to stdout.
  * Return:0 if success.
  */
+
 int main(void)
 {
 	put ("\"Programming is like building a multilingual puzzle");
-	return(0)
+	return(0);
 }
