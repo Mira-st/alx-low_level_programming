67364a8e7074381afa3820da3912c1e71af5560b
orirekemi <orirekemi64@gmail.com>
2022-06-30 08:48:20 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry Point
- * Description: A C program that prints a string using puts.
- * Return:0 - success.
+ * Description: main - print out a string to stdout.
+ * Return: 0 if success.
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
