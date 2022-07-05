d0ed455b3b0cefc456beeb96832a068ce29a87d9
orirekemi <orirekemi64@gmail.com>
2022-07-05 06:52:10 -0700

    print

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,10 @@
  */
 int print_last_digit(int n)
 {
-	int x;
+	n = n % 10;
 
 	if (n < 0)
 		n = -n;
-	return (n % 10);
-	x = n % 10;
-	_putchar(x + '0');
-	return (x);
+	_putchar(n + '0');
+	return (n);
 }
