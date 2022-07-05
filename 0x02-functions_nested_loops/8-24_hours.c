4248ac17d29ff96600672e444798e7c16d979e13
orirekemi <orirekemi64@gmail.com>
2022-07-05 07:20:17 -0700

    loop

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int m, n;
+
+	for (m = 0; m < 24; m++)
+	{
+		for (n = 0; n < 60; n++)
+		{
+			if (m < 10)
+			{
+				_putchar('0);
+				_putchar(m + '0')
+			}
+			else if (m >= 10)
+			{
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+			}
+			if (n < 10)
+			{
+				_putchar(' : ');
+				_putchar('0');
+				_putchar(n);
+			}
+			else if (n > 10)
+			{
+				_putchar( ' : ' );
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+		}
+	}
+
+}
