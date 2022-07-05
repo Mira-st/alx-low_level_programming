28129815d53ff12d8bbb26dcbbf51a9c19423837
orirekemi <orirekemi64@gmail.com>
2022-07-05 06:26:44 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs -> return absolute value of a number
+ * @g: parameter
+ * Return: absolute value of a number
+ */
+int _abs(int g)
+{
+	if (g < 0)
+		g = -g;
+	return (g);
+}
