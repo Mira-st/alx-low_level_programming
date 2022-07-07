2d76b435c4a22d16a44acb3fe2f72dcd8944a686
orirekemi <orirekemi64@gmail.com>
2022-07-07 15:02:58 -0700

    line

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 int _isupper(int c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -8,5 +9,6 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
 
 #endif
