3ff6b12f59dd35f5f65512a387c88ebefc82b610
orirekemi <orirekemi64@gmail.com>
2022-07-07 16:55:40 -0700

    tap

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,7 +6,17 @@
  */
 void print_number(int n)
 {
+	unsigned int num = n;
+
+	/*first check if its negative*/
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
 
-	return (0);
+	_putchar((num % 10) + 48);
 }
