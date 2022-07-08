3b7bef2c892dbcf6440ce3a27f8601b920f2f957
orirekemi <orirekemi64@gmail.com>
2022-07-07 18:25:22 -0700

    test

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,18 +6,27 @@
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int m, d, count;
 
-	/*first check if its negative*/
 	if (n < 0)
 	{
-		_putchar('-');
-		num = -num;
+		_putchar(45);
+		m = n * -1;
 	}
-	/*print the first few digits*/
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	else
+	{
+		m = n;
+	}
+	d = m;
+	count = 1;
 
-	/*print the last digit*/
-	_putchar((num % 10) + 48);
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
