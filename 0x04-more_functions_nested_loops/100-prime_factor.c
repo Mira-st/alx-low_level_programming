1ca1bc4c51e0eb115e0dafce92e77534baf54fae
orirekemi <orirekemi64@gmail.com>
2022-07-07 18:18:54 -0700

    hey

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,6 +7,17 @@
  */
 int main(void)
 {
+	long int n, pf;
 
+	n = 612852475143;
+	for (pf = 2; pf <= n; pf++)
+	{
+		if (n % pf == 0)
+		{
+			n /= pf;
+			pf--;
+		}
+	}
+	printf("%ld\n", pf);
 	return (0);
 }
