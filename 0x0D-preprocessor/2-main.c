43a6058b8925f9a0daa636622166ef2ac184c43d
orirekemi <orirekemi64@gmail.com>
2022-08-01 09:25:19 -0700

    file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * main - function that prints the name of the file it was complied from
+ *
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
