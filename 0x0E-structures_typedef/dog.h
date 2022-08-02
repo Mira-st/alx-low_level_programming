07d0fba765aa967b67787a6ddc486e00fecf1143
orirekemi <orirekemi64@gmail.com>
2022-08-02 03:29:36 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,22 +2,21 @@
 #define DOG_H
 
 /**
- * struct -> function to typedef of stuct dog
+ * struct dog - function to typedef struct dog
  * @name: character pointer
- * @age: float pointer
+ * @age: float
  * @owner: character pointer
- * Return: 0 Always(success)
  */
-
-typedef struct dog 
+typedef struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 
 #endif
