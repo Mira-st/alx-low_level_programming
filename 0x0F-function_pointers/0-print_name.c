079eb66f94a14fa50d22123eb9fb649677f89f54
orirekemi <orirekemi64@gmail.com>
2022-08-03 05:59:37 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_name -> function to print a name
+ * @name: character pointer
+ * @f: pointer
+ * Return: 0 Always
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	void main (char f)
+		char *f;
+	printf("My name is Orire Kemi\n", f);
+	getchar();
+
+	return (0);
+}
