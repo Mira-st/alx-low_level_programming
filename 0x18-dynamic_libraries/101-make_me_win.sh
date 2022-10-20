b4fdd7e9f0c18b7b3c28123332936fa1dc5749f4
Mira-st <orirekemi64@gmail.com>
2022-10-20 11:39:11 -0700

    Uodate

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -q -o /tmp/cheatme.so https://github.com/rolandoquiroz/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/cheatme.so
-export LD_PRELOAD=/tmp/cheatme.so
-
+wget -P /tmp/ http://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
